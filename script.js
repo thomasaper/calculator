e9bb38dfe1991989a2d014466c93265f8ff5283c
Thomas Aper <thomasaper@gmx.de>
2022-11-15 15:40:41 +0100

    optimized math functions

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
-const sum = function(...array) {
+// Basic math functions
 
-  return array.reduce((a, b) => a + b);
-  
+const sum = function(...array) {
+  return array.reduce((a, b) => a + b);  
 };
 
 
 const subtract = function(...array) {
-
   return array.reduce((a, b) => a - b);
-
 };
 
 
 const multiply = function(...array) {
-
   return array.reduce((a, b) => a * b, 1);
-
 };
 
 
 const divide = function(...array) {
-
   return array.reduce((a, b) => a / b);
-  
 };
 
 const power = function(a, b) {
-
   return Math.pow(a, b);
 };
 
 
 const factorial = function(a) {
-
   if (a === 0 || a === 1) return 1;
-
   for (let i = a - 1; i >= 1; i--){
     a*=i;
   };
-
   return a;
 };
+
